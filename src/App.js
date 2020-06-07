21c0e89284ae8b74d2ebba6c2ae1361ffc28f6d9
ravinath <tudu.ravinath@gmail.com>
2020-06-07 20:31:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './components/pages/Contact';
 import About from './components/pages/About';
 import Navbar from './components/layout/Navbar';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import NotFound from './components/pages/NotFound';
+// import NotFound from './components/pages/NotFound';
 import AddUser from './components/users/AddUser';
 import Edituser from './components/users/EditUser';
 import UserView from './components/users/UserView';
@@ -16,14 +16,16 @@ function App() {
     <Router>
       <div className="App">
       <Navbar></Navbar>
+      <header className="App-header">
+       <h1>Simple CURD Operation in React</h1>       
+      </header>
       <Switch>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/Contact" component={Contact}></Route>
         <Route exact path="/About" component={About}></Route>
         <Route exact path="/user/add" component={AddUser}></Route>
         <Route exact path="/user/edit/:id" component={Edituser}></Route>
-        <Route exact path="/user/view/:id" component={UserView}></Route>
-        <Route component={NotFound}></Route>
+        <Route exact path="/user/view/:id" component={UserView}></Route>        
       </Switch>      
     </div>
     </Router>
