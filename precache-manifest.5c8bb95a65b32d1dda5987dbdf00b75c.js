804ff8387440355d9e5bef0817a0d22f589b16f7
ravinath <tudu.ravinath@gmail.com>
2020-06-07 19:47:43 +0530

    Updates

---
+++ precache-manifest.5c8bb95a65b32d1dda5987dbdf00b75c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d3958c1e03df7236b4bfe60e72e4dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc8145d8725361ab704",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "701d65c088d32f072e6a",
+    "url": "/static/css/main.b0dfc659.chunk.css"
+  },
+  {
+    "revision": "0dc8145d8725361ab704",
+    "url": "/static/js/2.a9373c94.chunk.js"
+  },
+  {
+    "revision": "ab2b2c61a272426d88c20c7468be148d",
+    "url": "/static/js/2.a9373c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701d65c088d32f072e6a",
+    "url": "/static/js/main.7449f216.chunk.js"
+  },
+  {
+    "revision": "be34b7d4289cff8d5df7",
+    "url": "/static/js/runtime-main.c7528935.js"
+  }
+]);
